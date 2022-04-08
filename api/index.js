09f1699914f8617c30021a653734d66b579058d7
facundo2309 <facundoomargarcia@gmail.com>
2022-04-08 14:33:51 -0300

    conexion server y base de datos

---
+++ api/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const server=require('./src/app')
+const{ conn }= require('./src/db')
+
+const port=process.env.PORT||8008
+
+conn.sync({force:false})
+.then(()=>server.listen(
+    port,
+    ()=>console.log(`server listening at port ${port} http://localhost:${port}`)
+    )
+)
+
